944648c77afebfa95795752fd51d91a6a5a3ae17
gkze <george@chartboost.com>
2014-08-22 14:02:44 -0700

    Refactor

--- ldap_script.py
+++ ldap_script.py
@@ -7,22 +7,46 @@ def get_user_data():
     uidNumberFilePath = '/tmp/uidNumber '
 
     #apass = raw_input('Enter LDAP auth password: ')
-    file = raw_input('Enter filename: ')
+    filename = raw_input('Enter filename: ')
     givenName = raw_input('Enter first name: ')
     sn = raw_input('Enter last name: ')
     cn = raw_input('Enter username: ')
     mail = raw_input('Enter email: ')
     title = raw_input('Enter title: ')
     userpass = raw_input('Enter user password: ')
-    uidNumber = os.popen("ldapsearch -x -LLL -H ldap://ldap-utility-e1a-001.caffeine.io -b dc=caffeine,dc=io 'uidNumber=*' -S uidNumber | awk '/uidNumber/ {print $2}' | tail -n1 >" + uidNumberFilePath)
+    uidNumber = os.popen("ldapsearch -x -LLL -H ldap://ldap-utility-e1a-001.caffeine.io -b dc=caffeine,dc=io 'uidNumber=*' -S uidNumber | awk '/uidNumber/ {print $2}' | tail -n1 >")
 
-    return {'first': givenName, 'last': sn, 'username': cn, 'email': mail, 'title': title, 'filename': file, 'userpass': userpass, 'uidNumber': uidNumber, 'uidNumberFilePath': uidNumberFilePath}
+    return {'first': givenName,
+            'last': sn,
+            'username': cn,
+            'email': mail,
+            'title': title,
+            'filename': filename,
+            'userpass': userpass,
+            'uidNumber': uidNumber,
+            'uidNumberFilePath': uidNumberFilePath}
 
 
 def main():
     data = get_user_data()
-    entry={'objectClass':['top', 'person', 'organizationalPerson', 'inetOrgPerson', 'posixAccount'], 'cn': [data['username']], 'givenName': [data['first']], 'uid': [data['username']], 'mail': [data['email']], 'title': [data['title']], 'gecos': [data['username']], 'homeDirectory': ['/home/' + data['username']], 'loginShell': ['/bin/bash'], 'userPassword': [data['userpass']], 'uidNumber': [int(open('uidNumberFilePath', 'r').read().strip())+1]}
-    dn='cn=' + data['username'] + ',ou=chartboost' + ',dc=caffeine,dc=io'
+
+    entry = {'objectClass': ['top',
+                             'person',
+                             'organizationalPerson',
+                             'inetOrgPerson',
+                             'posixAccount'],
+             'cn': [data['username']],
+             'givenName': [data['first']],
+             'uid': [data['username']],
+             'mail': [data['email']],
+             'title': [data['title']],
+             'gecos': [data['username']],
+             'homeDirectory': ['/home/' + data['username']],
+             'loginShell': ['/bin/bash'],
+             'userPassword': [data['userpass']],
+             'uidNumber': [data['uidNumber']]}
+
+    dn = 'cn=' + data['username'] + ',ou=chartboost' + ',dc=caffeine,dc=io'
     with open('/tmp/' + data['filename'] + '.ldif', 'w') as fd:
         ldif_writer = ldif.LDIFWriter(fd)
         ldif_writer.unparse(dn, entry)
